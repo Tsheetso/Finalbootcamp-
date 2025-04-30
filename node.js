0313697d80ded9e243a2db4e0730e9f8e300a719
Tsheetso <tboledy@gmail.com>
2025-04-30 15:04:18 +0200

    Add files via upload

---
+++ node.js
@@ -0,0 +1,10 @@
+// Wait until the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+    const signupButton = document.getElementById("signup-button");
+  
+    signupButton.addEventListener("click", function () {
+      alert("Thank you for signing up!");
+      // You can add redirect or form submission here
+    });
+  });
+  
